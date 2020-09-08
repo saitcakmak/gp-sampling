7a438c819e1d672f693f38c81c78c0d05dac2187
saitcakmak <saitcakmak@outlook.com>
2020-09-08 12:58:34 -0400

    refactoring

---
+++ gp_sampling/__init__.py
@@ -0,0 +1,20 @@
+from gp_sampling import utils
+from gp_sampling.base import Sampler, CompositeSampler, BayesianLinearSampler
+from gp_sampling.basis_functions import Basis, KernelBasis, RandomFourierBasis
+from gp_sampling.decoupled_samplers import decoupled_sampler
+from gp_sampling.thompson_samplers import decoupled_ts, exact_ts, continuous_decoupled_ts
+
+
+__all__ = [
+    "utils",
+    "Sampler",
+    "CompositeSampler",
+    "BayesianLinearSampler",
+    "Basis",
+    "KernelBasis",
+    "RandomFourierBasis",
+    "decoupled_sampler",
+    "decoupled_ts",
+    "exact_ts",
+    "continuous_decoupled_ts",
+]
---
+++ gp_sampling/utils/__init__.py
@@ -0,0 +1,7 @@
+from gp_sampling.utils.random_gp import generate_random_gp
+from gp_sampling.utils.layer import Layer
+
+__all__ = [
+    "generate_random_gp",
+    "Layer",
+]
